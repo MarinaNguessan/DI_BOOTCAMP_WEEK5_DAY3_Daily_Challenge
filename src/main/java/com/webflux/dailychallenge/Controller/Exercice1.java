f1cf61b3e98d90bf621aeb2c11038ec74603fb9d
MarinaNguessan <marinanguessan23@gmail.com>
2023-03-12 10:40:22 +0000

    first commit

---
+++ src/main/java/com/webflux/dailychallenge/Controller/Exercice1.java
@@ -0,0 +1,7 @@
+package com.webflux.dailychallenge.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Exercice1 {
+    
+}
---
+++ src/main/java/com/webflux/dailychallenge/DailychallengeApplication.java
@@ -0,0 +1,13 @@
+package com.webflux.dailychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailychallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailychallengeApplication.class, args);
+	}
+
+}
